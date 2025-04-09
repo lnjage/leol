b830a9c67eae9bfb2db2327f19f8817732c911f9
lnjage <lnjanja@outlook.com>
2025-04-09 13:41:30 +0300

    why choose us

---
+++ components/Home/WhyChoose/WhyChoose.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const WhyChoose = () => {
+  return (
+    <div className = "pt-16 pb-16">
+        <h1 className='mt-6 text-2xl md:text-3xl capitalize font-bold text-center'> 
+            Why you Should Choose Us
+        </h1>
+        <div className='mt-20 grid w-[90%] mx-auto grid-cols-1 md:gris-cols-2 lg:gris cols-3 xl:grid:cols-4 gap-12'>
+        <div/>
+ </div>
+   </div>
+  )
+}
+
+export default WhyChoose

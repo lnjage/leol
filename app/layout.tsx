255e2aca45b14d2f5d2c7c02c076d207b2d15b52
lnjage <lnjanja@outlook.com>
2025-05-04 04:24:24 +0300

    woiiii

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,17 @@
 import type { Metadata } from "next";
-//import { Geist, Geist_Mono } from "next/font/google";
-import {Poppins} from 'next/font/google'
+import { Poppins } from 'next/font/google';
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 
-
-
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
 const font = Poppins ({
   weight: ["100","300","400","500","700","900","200","800"],
-  subsets:["latin"],
-})
+  subsets: ["latin"],
+});
+
 export const metadata: Metadata = {
   title: "Leol Landing Page | Next 18",
   description: "App Landing Page using Next Js 18",
- };
+};
 
 export default function RootLayout({
   children,
@@ -33,11 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${font.className} antialiased`}>
-          <ResponsiveNav/>
-        {children}
+      <head>
         
+        <base href="/leol/" />
+      </head>
+      <body className={`${font.className} antialiased`}>
+        <ResponsiveNav />
+        {children}
       </body>
     </html>
   );
